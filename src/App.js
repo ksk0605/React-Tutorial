31ac7ab657b6e16538c36a42d567a0d455a69aa1
kimsungkyum <sungkyum1@naver.com>
2023-04-13 01:53:28 +0900

    React 2022 개정판 1-6 수강

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
-
+function Header(props) {
+  console.log(props)
+  
+  return <header>
+          <h1><a href='/' onClick={(event) => {
+            event.preventDefault();
+            props.onChangeMode();
+          }}>{props.title}</a></h1>
+        </header> 
+}
+function Nav(props) {
+  const lis = []
+  props.topics.map(x => lis.push(<li key={x.id}>
+    <a id={x.id} href={'/read/' + x.id} onClick={event => {event.preventDefault(); props.onChangeMode(event.target.id);}}>{x.title}</a>
+    </li>))
+  return <nav>
+          <ol>
+            {lis}
+          </ol>
+        </nav>
+}
+function Article(props) {
+  return <article>
+          <h2>{props.title}</h2>
+          {props.body}
+        </article>
+}
 function App() {
+  const topics = [
+    {id:1, title:'html' , body:'html is ...'},
+    {id:2, title:'css' , body:'css is ...'},
+    {id:3, title:'js' , body:'js is ...'},
+  ]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header title="WEB" onChangeMode={() =>{alert('header')}}></Header> 
+      <Nav topics ={topics} onChangeMode={(id)=>{alert(id)}}></Nav>
+      <Article title='Welecome' body='Hello!!!'></Article>
     </div>
   );
 }
