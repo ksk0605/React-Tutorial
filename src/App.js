074cc15fbb223f109ac8937d236d75c3c962ae79
kimsungkyum <sungkyum1@naver.com>
2023-04-17 23:18:23 +0900

    7강 state 개념 수강

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import { useState } from 'react';
+
 function Header(props) {
   console.log(props)
   
@@ -11,9 +13,17 @@ function Header(props) {
 }
 function Nav(props) {
   const lis = []
-  props.topics.map(x => lis.push(<li key={x.id}>
-    <a id={x.id} href={'/read/' + x.id} onClick={event => {event.preventDefault(); props.onChangeMode(event.target.id);}}>{x.title}</a>
-    </li>))
+  props.topics.map(x => lis.push(
+  <li key={x.id}>
+    <a id={x.id} href={'/read/' + x.id} onClick={
+      event => {event.preventDefault(); 
+      props.onChangeMode(Number(event.target.id));}
+      }
+    >
+      {x.title}
+    </a>
+    </li>)
+    )
   return <nav>
           <ol>
             {lis}
@@ -27,16 +37,40 @@ function Article(props) {
         </article>
 }
 function App() {
+//   const _mode = useState('WELCOME');
+//   const mode = _mode[0]; // state의 초깃값은 state의 0번째 값
+//   const setMode = _mode[1];
+  const [mode, setMode] = useState('WELCOME'); // 위 3줄과 같은 의미 but 가장 자주 사용하는 문법
+  const [id, setId] = useState(null);
+
   const topics = [
     {id:1, title:'html' , body:'html is ...'},
     {id:2, title:'css' , body:'css is ...'},
     {id:3, title:'js' , body:'js is ...'},
   ]
+
+  let content = null;
+  if(mode === 'WELCOME') {
+    content = <Article title='Welecome' body='Hello, WEB'></Article>;
+  } else if(mode === 'READ') {
+    let title, body = null;
+    topics.forEach((topic) => {
+      if (topic.id === id){
+        title = topic.title;
+        body = topic.body;
+        content = <Article title={title} body={body}></Article>;
+      }
+    })
+  }
+
   return (
     <div className="App">
-      <Header title="WEB" onChangeMode={() =>{alert('header')}}></Header> 
-      <Nav topics ={topics} onChangeMode={(id)=>{alert(id)}}></Nav>
-      <Article title='Welecome' body='Hello!!!'></Article>
+      <Header title="WEB" onChangeMode={() =>{setMode('WELCOME')}}></Header> 
+      <Nav topics ={topics} onChangeMode={(_id)=>{
+        setMode('READ');
+        setId(_id);
+        }}></Nav>
+      {content}
     </div>
   );
 }
