72c639e6c29864c1dad6f98e7870c614805552c0
kimsungkyum <sungkyum1@naver.com>
2023-04-19 12:29:37 +0900

    8강 Creaet

--- src/App.js
+++ src/App.js
@@ -36,18 +36,35 @@ function Article(props) {
           {props.body}
         </article>
 }
+function Create(props) {
+  return <article>
+    <h2>Create</h2>
+    <form onSubmit={event=>{
+      event.preventDefault();
+      const title = event.target.title.value;
+      const body = event.target.body.value;
+      props.onCreate(title,body);
+    }}>
+      <p><input type='text' name='title' placeholder='title'></input></p>
+      <p><textarea name='body' placeholder='body'></textarea></p>
+      <p><input type='submit' value='Create'></input></p>
+    </form>
+  </article>
+}
+
 function App() {
 //   const _mode = useState('WELCOME');
 //   const mode = _mode[0]; // state의 초깃값은 state의 0번째 값
 //   const setMode = _mode[1];
   const [mode, setMode] = useState('WELCOME'); // 위 3줄과 같은 의미 but 가장 자주 사용하는 문법
   const [id, setId] = useState(null);
+  const [nextId, setNextid] = useState(4);
 
-  const topics = [
+  const [topics, setTopics] = useState([
     {id:1, title:'html' , body:'html is ...'},
     {id:2, title:'css' , body:'css is ...'},
     {id:3, title:'js' , body:'js is ...'},
-  ]
+  ]);
 
   let content = null;
   if(mode === 'WELCOME') {
@@ -61,6 +78,15 @@ function App() {
         content = <Article title={title} body={body}></Article>;
       }
     })
+  } else if (mode === 'CREATE') {
+    content = <Create onCreate={(_title, _body) => {
+      const newTopics = [...topics];
+      newTopics.push({id:nextId, title: _title, body: _body});
+      setTopics(newTopics);
+      setId(nextId);
+      setNextid(nextId+1);
+      setMode('READ')
+    }}></Create>
   }
 
   return (
@@ -71,6 +97,12 @@ function App() {
         setId(_id);
         }}></Nav>
       {content}
+      <a href='/create' onClick={event=>{
+        event.preventDefault();
+        setMode('CREATE');
+      }}>
+        Create
+      </a>
     </div>
   );
 }
